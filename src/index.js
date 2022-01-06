c6beed4344fd16456a8ed64504780573e1cb9524
Leandro <leandroquiroga9514@gmail.com>
2022-01-06 15:25:08 -0300

    Init App

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDom from "react-dom";
 import CounterAPP from "./CounterApp";
 import './index.css'
 
-const root = document.querySelector('#root')
-
+const root = document.querySelector('#root');
 ReactDom.render(<CounterAPP value={ 0 } />, root)
